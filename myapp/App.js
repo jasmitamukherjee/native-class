5a5105acad01a1dd41b654dbe40734a2dd44efe4
Jasmita Mukherjee <125654683+jasmitamukherjee@users.noreply.github.com>
2024-03-05 10:30:24 +0530

    /created button to increment and decrement

--- myapp/App.js
+++ myapp/App.js
@@ -1,11 +1,44 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hi jasmita</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+//new add :
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [counter,setCounter] = useState(0)
+
+  function increament(){
+    setCounter(counter+1)
+  }
+  function decreament(){
+    setCounter(counter-1)
+  }
   return (
     <View style={styles.container}>
-      <Text>Hi jasmita</Text>
+      <Text>Counter= {counter}</Text>
       <StatusBar style="auto" />
+      <Button title="increament" onPress={increament} color={'green'}></Button>
+      <Button title="decreament" onPress={decreament} color={'red'}></Button>
     </View>
   );
 }
