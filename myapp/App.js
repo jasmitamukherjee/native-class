983cb7075897e114bfdeb7f258ea436368066333
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-12 13:04:42 +0530

    updated

--- myapp/App.js
+++ myapp/App.js
@@ -1,25 +1,4 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
 
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Hi jasmita</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-//new add :
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
@@ -33,12 +12,17 @@ export default function App() {
   function decreament(){
     setCounter(counter-1)
   }
+  function handleclick(){
+    alert("hey")
+  }
   return (
     <View style={styles.container}>
+      <Text>Hi Jasmita!</Text>
       <Text>Counter= {counter}</Text>
       <StatusBar style="auto" />
-      <Button title="increament" onPress={increament} color={'green'}></Button>
-      <Button title="decreament" onPress={decreament} color={'red'}></Button>
+      <Button title="up" onPress={increament} color={'green'}></Button>
+      <Button title="down" onPress={decreament} color={'red'}></Button>
+      <Button title='Click me!' onPress={handleclick} color={'black'}></Button>
     </View>
   );
 }
